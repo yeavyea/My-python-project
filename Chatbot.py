fcf442657881a6c82054bf9b7039b2dc91c26409
yeavyea <yeavyea@api-school.com>
2025-06-02 09:00:46 +0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ hobbie = input ("What is your favorite thing to do?")
 time.sleep(2)
 print("That sounds great, I really love", random.choice(chatHobbies))
 day = input("Have you had a good day?")
-day = day.lower
+day = day.lower()
 if day == "yes":
     print("Good to hear that, me2!")
 else:
