190742df54c1faf28a7fd15630ab13181fc75700
yeavyea <yeavyea@api-school.com>
2025-07-16 07:51:58 +0700

    Create Flowchart.py

---
+++ Flowchart.py
@@ -0,0 +1,13 @@
+answer = input("Do you wanna hangout this weekend? Y/N")
+if answer == "Y":
+print("Wa ho!")
+else:
+answer = input ("Okay, what about next weekend? Y/N")
+if answer == "Y":
+print("Oh yes!")
+else:
+answer = input ("Alright, are you free this friday? Y/N")
+if answer == "Y":
+print("Let's go hangout then!")
+else:
+print("You need to take a break now!")
